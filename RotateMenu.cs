5891dd19dd916c14092d2d76fb2746db4d2dfc25
Bambino19 <58581193+Bambino19@users.noreply.github.com>
2019-12-05 22:59:55 -0500

    Add files via upload

---
+++ RotateMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMenu : MonoBehaviour
+{
+    public GameObject Canvas;
+
+    void Start()
+    {
+        Debug.Log("Executed");
+        Canvas = GameObject.Find("MainMenuManager");
+        Canvas.transform.position = new Vector3(30, 7, -10);
+        Canvas.transform.localEulerAngles = new Vector3(0, 90, 90);
+        //Canvas.transform.Rotate(0, 0, 90);
+        //MainMenu.GetComponent<RectTransform>().localPosition = Canvas;
+    }
+}
